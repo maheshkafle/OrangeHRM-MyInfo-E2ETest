d97f0018a954c2455700833934ec02a76d6677fe
kaflemahesh <mahesh.kafle@smarsh.com>
2021-09-20 23:57:40 +0545

    Added test cases for Dashboard Page

--- src/main/java/com/orangehrm/qa/pages/DashboardPage.java
+++ src/main/java/com/orangehrm/qa/pages/DashboardPage.java
@@ -1,4 +1,27 @@
 package com.orangehrm.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static com.orangehrm.qa.base.TestBase.driver;
+
 public class DashboardPage {
+
+    @FindBy(xpath = "//h1[contains(text(), 'Dashboard')]")
+    WebElement dashboardTitle;
+
+    // Initializing Page Objects using constructor
+    public DashboardPage(){
+        PageFactory.initElements(driver, this);
+    }
+
+    // Action
+    public boolean validateDashboardLogo(){
+        Boolean flag = dashboardTitle.isDisplayed();
+        return flag;
+    }
+
+
+
 }
