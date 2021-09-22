811bcc45dac659df8be3c5fd29700dd22980fd2b
kaflemahesh <mahesh.kafle@smarsh.com>
2021-09-22 23:39:45 +0545

    Implemented PersonalDetails Page Test

--- src/main/java/com/orangehrm/qa/pages/PersonalDetailsPage.java
+++ src/main/java/com/orangehrm/qa/pages/PersonalDetailsPage.java
@@ -1,5 +1,58 @@
 package com.orangehrm.qa.pages;
 
-public class PersonalDetailsPage {
+import com.orangehrm.qa.base.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PersonalDetailsPage extends TestBase {
+
+    @FindBy(xpath = "//a[contains(text(), 'Contact Details')]")
+    WebElement ContactDetailsPageLink;
+
+    @FindBy(xpath = "//a[contains(text(), 'Emergency Contacts')]")
+    WebElement EmergencyContactsPageLink;
+
+    @FindBy(xpath = "//a[contains(text(), 'Dependents')]")
+    WebElement DependentsPageLink;
+
+    @FindBy(xpath = "//a[contains(text(), 'Immigration')]")
+    WebElement ImmigrationPageLink;
+
+    @FindBy(xpath = "//a[contains(text(), 'Job')]")
+    WebElement JobPageLink;
+
+
+    // Initializing Page Objects using constructor
+    public PersonalDetailsPage(){
+        PageFactory.initElements(driver, this);
+    }
+
+    // Action
+    public ContactDetailsPage clickOnContactDetailsPageLink(){
+        ContactDetailsPageLink.click();
+        return new ContactDetailsPage();
+    }
+
+    public EmergencyContactsPage clickOnEmergencyContactsPageLink(){
+        EmergencyContactsPageLink.click();
+        return new EmergencyContactsPage();
+    }
+
+    public DependentsPage clickOnDependentsPageLink(){
+        DependentsPageLink.click();
+        return new DependentsPage();
+    }
+
+    public ImmigrationPage clickOnImmigrationPageLink(){
+        ImmigrationPageLink.click();
+        return new ImmigrationPage();
+    }
+
+    public JobPage clickOnJobPageLink(){
+        JobPageLink.click();
+        return new JobPage();
+    }
 
 }
