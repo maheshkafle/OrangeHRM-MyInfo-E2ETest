08098aadcf07edfc0f06476331cf259c756fc2c1
kaflemahesh <mahesh.kafle@smarsh.com>
2021-09-24 00:53:45 +0545

    Implemented verify ESS user can edit contact details field

--- src/main/java/com/orangehrm/qa/pages/ContactDetailsPage.java
+++ src/main/java/com/orangehrm/qa/pages/ContactDetailsPage.java
@@ -1,4 +1,104 @@
 package com.orangehrm.qa.pages;
 
-public class ContactDetailsPage {
+import com.orangehrm.qa.base.TestBase;
+import com.orangehrm.qa.utils.TestUtil;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ContactDetailsPage extends TestBase {
+
+    // Object Repo
+    @FindBy(id = "btnSave")
+    WebElement editSaveBtn;
+
+    @FindBy(id = "contact_street1")
+    WebElement Street1;
+
+    @FindBy(id = "contact_street2")
+    WebElement Street2;
+
+    @FindBy(id = "contact_city")
+    WebElement City;
+
+    @FindBy(id = "contact_province")
+    WebElement Province;
+
+    @FindBy(id = "contact_emp_zipcode")
+    WebElement PostalCode;
+
+    @FindBy(id = "contact_emp_hm_telephone")
+    WebElement HomeTelephone;
+
+    @FindBy(id = "contact_emp_mobile")
+    WebElement EmployeeMobileNo;
+
+    @FindBy(id = "contact_emp_work_telephone")
+    WebElement EmployeeWorkTelephoneNo;
+
+    @FindBy(id = "contact_emp_work_email")
+    WebElement EmployeeWorkEmail;
+
+    @FindBy(id = "contact_emp_oth_email")
+    WebElement EmployeeOtherEmail;
+
+    @FindBy(className = "message")
+    WebElement SuccessMessage;
+
+    // Initializing Page Objects using constructor
+    public ContactDetailsPage(){
+        PageFactory.initElements(driver, this);
+    }
+
+    // Actions
+    public void clickOnEditBtn(){
+        editSaveBtn.click();
+    }
+
+    public void editStreet1(String street1){
+        TestUtil.doSendKeys(Street1, street1);
+    }
+
+    public void editStreet2(String street2){
+        TestUtil.doSendKeys(Street2, street2);
+    }
+
+    public void editCity(String city){
+        TestUtil.doSendKeys(City, city);
+    }
+
+    public void editProvince(String province){
+        TestUtil.doSendKeys(City, province);
+    }
+
+    public void editPostalCode(String postalcode){
+        TestUtil.doSendKeys(PostalCode, postalcode);
+    }
+
+    public void editHomeTelephone(String hometelephone){
+        TestUtil.doSendKeys(HomeTelephone, hometelephone);
+    }
+
+    public void editEmployeeMobileNo(String employeemobileno){
+        TestUtil.doSendKeys(EmployeeMobileNo, employeemobileno);
+    }
+
+    public void editEmployeeWorkTelephoneNo(String employeeworktelephoneno){
+        TestUtil.doSendKeys(EmployeeWorkTelephoneNo, employeeworktelephoneno);
+    }
+
+    public void editEmployeeWorkEmail(String employeeworkemail){
+        TestUtil.doSendKeys(EmployeeWorkEmail, employeeworkemail);
+    }
+
+    public void editEmployeeOtherEmail(String employeeotheremail){
+        TestUtil.doSendKeys(EmployeeOtherEmail, employeeotheremail);
+    }
+
+    public boolean isContactDetailsEdited(){
+        System.out.println(SuccessMessage.getText());
+        Boolean flag = SuccessMessage.isDisplayed();
+        return flag;
+    }
+
 }
--- src/test/java/com/orangehrm/qa/testcases/ContactDetailsPageTest.java
+++ src/test/java/com/orangehrm/qa/testcases/ContactDetailsPageTest.java
@@ -1,9 +1,11 @@
 package com.orangehrm.qa.testcases;
 
 import com.orangehrm.qa.base.TestBase;
+import com.orangehrm.qa.pages.ContactDetailsPage;
 import com.orangehrm.qa.pages.DashboardPage;
 import com.orangehrm.qa.pages.LoginPage;
 import com.orangehrm.qa.pages.PersonalDetailsPage;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,6 +15,7 @@ public class ContactDetailsPageTest extends TestBase {
     PersonalDetailsPage personalDetailsPage;
     DashboardPage dashboardPage;
     LoginPage loginPage;
+    ContactDetailsPage contactDetailsPage;
 
     public ContactDetailsPageTest(){
         super();
@@ -24,13 +27,29 @@ public class ContactDetailsPageTest extends TestBase {
         loginPage = new LoginPage();
         dashboardPage = new DashboardPage();
         personalDetailsPage = new PersonalDetailsPage();
+        contactDetailsPage = new ContactDetailsPage();
         dashboardPage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
         personalDetailsPage = dashboardPage.clickOnPersonalDetailsPageLink();
+        contactDetailsPage = personalDetailsPage.clickOnContactDetailsPageLink();
     }
 
     @Test(priority = 1)
-    public void verifyNavigateToPersonalDetailsPage(){
-        personalDetailsPage.clickOnContactDetailsPageLink();
+    public void verifyEditContactDetails() throws InterruptedException {
+        contactDetailsPage.clickOnEditBtn();
+        contactDetailsPage.editStreet1(prop.getProperty("street1"));
+        contactDetailsPage.editStreet2(prop.getProperty("street2"));
+        contactDetailsPage.editCity(prop.getProperty("city"));
+        contactDetailsPage.editProvince(prop.getProperty("province"));
+        contactDetailsPage.editPostalCode(prop.getProperty("postalcode"));
+        contactDetailsPage.editHomeTelephone(prop.getProperty("hometelephone"));
+        contactDetailsPage.editEmployeeMobileNo(prop.getProperty("employeemobileno"));
+        contactDetailsPage.editEmployeeWorkTelephoneNo(prop.getProperty("employeeworktelephoneno"));
+        contactDetailsPage.editEmployeeWorkEmail(prop.getProperty("employeeworkemail"));
+        contactDetailsPage.editEmployeeOtherEmail(prop.getProperty("employeeotheremail"));
+        contactDetailsPage.clickOnEditBtn();
+        Boolean flag = contactDetailsPage.isContactDetailsEdited();
+        Assert.assertTrue(flag);
+        Thread.sleep(2000);
     }
 
     @AfterMethod
