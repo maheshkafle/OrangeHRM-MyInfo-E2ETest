12718123e456706610f590754e2274b3bed01d19
kaflemahesh <mahesh.kafle@smarsh.com>
2021-09-26 02:03:52 +0545

    Implemented ESS user can add or delete Work Experience Records

--- src/main/java/com/orangehrm/qa/pages/QualificationsPage.java
+++ src/main/java/com/orangehrm/qa/pages/QualificationsPage.java
@@ -1,4 +1,92 @@
 package com.orangehrm.qa.pages;
 
-public class QualificationsPage {
+import com.orangehrm.qa.base.TestBase;
+import com.orangehrm.qa.utils.TestUtil;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class QualificationsPage extends TestBase {
+
+    // Object Repo
+    @FindBy(id = "addWorkExperience")
+    WebElement BtnAddWorkExperience;
+
+    @FindBy(id = "btnWorkExpSave")
+    WebElement BtnSaveWorkExperience;
+
+    @FindBy(id = "experience_employer")
+    WebElement Company;
+
+    @FindBy(id = "experience_jobtitle")
+    WebElement JobTitle;
+
+    @FindBy(id = "experience_from_date")
+    WebElement JobStartDate;
+
+    @FindBy(id = "experience_to_date")
+    WebElement JobEndDate;
+
+    @FindBy(id = "experience_comments")
+    WebElement Comments;
+
+    @FindBy(className = "message")
+    WebElement CheckStatusDiv;
+
+    //Hard coded xpath which contains text shi:dolutions: So pass accordingly
+    @FindBy(xpath = "//a[contains(text(), 'shi:dolutions')]/parent::td//preceding-sibling::td//input[@name='delWorkExp[]']")
+    WebElement WorkExpCheckbox;
+
+    @FindBy(id = "delWorkExperience")
+    WebElement BtndelWorkExperience;
+
+    // Initializing Page Objects using constructor
+    public QualificationsPage(){
+        PageFactory.initElements(driver, this);
+    }
+
+    public void clickAddWorkExperienceBtn(){
+        TestUtil.doClick(BtnAddWorkExperience);
+    }
+
+    public void AddCompany(String company){
+        TestUtil.doSendKeys(Company, company);
+    }
+
+    public void AddJobTitle(String jobTitle) throws InterruptedException {
+        TestUtil.doSendKeys(JobTitle, jobTitle);
+    }
+
+    public void AddJobStartDate(String jobStartDate) throws InterruptedException {
+        TestUtil.SelectDateFromCalenderCustom(driver, JobStartDate, jobStartDate);
+    }
+
+    public void AddJobEndDate(String jobEndDate) throws InterruptedException {
+        TestUtil.SelectDateFromCalenderCustom(driver, JobEndDate, jobEndDate);
+    }
+
+
+    public void AddComments(String comments){
+        TestUtil.doSendKeys(Comments, comments);
+    }
+
+    public void clickSaveWorkExperienceBtn(){
+        TestUtil.doClick(BtnSaveWorkExperience);
+    }
+
+    public boolean isWorkExperienceAdded() {
+        Boolean flag = TestUtil.checkStatus(CheckStatusDiv);
+        return flag;
+    }
+
+    public void deleteWorkExperience() {
+        TestUtil.doClick(WorkExpCheckbox);
+        TestUtil.doClick(BtndelWorkExperience);
+    }
+
+    public boolean isWorkExperienceDeleted() {
+        Boolean flag = TestUtil.checkStatus(CheckStatusDiv);
+        return flag;
+    }
+
 }
--- src/test/java/com/orangehrm/qa/testcases/QualificationsPageTest.java
+++ src/test/java/com/orangehrm/qa/testcases/QualificationsPageTest.java
@@ -4,7 +4,9 @@ import com.orangehrm.qa.base.TestBase;
 import com.orangehrm.qa.pages.DashboardPage;
 import com.orangehrm.qa.pages.LoginPage;
 import com.orangehrm.qa.pages.PersonalDetailsPage;
+import com.orangehrm.qa.pages.QualificationsPage;
 import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -14,6 +16,7 @@ public class QualificationsPageTest extends TestBase {
     PersonalDetailsPage personalDetailsPage;
     DashboardPage dashboardPage;
     LoginPage loginPage;
+    QualificationsPage qualificationsPage;
 
     public QualificationsPageTest(){
         super();
@@ -25,18 +28,36 @@ public class QualificationsPageTest extends TestBase {
         loginPage = new LoginPage();
         dashboardPage = new DashboardPage();
         personalDetailsPage = new PersonalDetailsPage();
+        qualificationsPage = new QualificationsPage();
         dashboardPage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
         personalDetailsPage = dashboardPage.clickOnPersonalDetailsPageLink();
+        JavascriptExecutor js = ((JavascriptExecutor) driver);
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        qualificationsPage = personalDetailsPage.clickOnQualificationsPageLink();
     }
 
     @Test(priority = 1)
-    public void verifyNavigateToJobPage() throws InterruptedException {
-
-        JavascriptExecutor js = ((JavascriptExecutor) driver);
-        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-
-        personalDetailsPage.clickOnQualificationsPageLink();
+    public void verifyAddWorkExperience() throws InterruptedException {
+        qualificationsPage.clickAddWorkExperienceBtn();
+        qualificationsPage.AddCompany(prop.getProperty("company"));
+        qualificationsPage.AddJobTitle(prop.getProperty("jobTitle"));
+        qualificationsPage.AddJobStartDate(prop.getProperty("jobStartDate"));
+        qualificationsPage.AddJobEndDate(prop.getProperty("JobEndDate"));
+        qualificationsPage.AddComments(prop.getProperty("workExpComments"));
+        qualificationsPage.clickSaveWorkExperienceBtn();
+        Boolean flag = qualificationsPage.isWorkExperienceAdded();
+        Assert.assertTrue(flag);
         Thread.sleep(3000);
+    }
+
+    @Test(priority = 2)
+    public void verifyDeleteImmigrants() throws InterruptedException {
+        for(int i=0; i<2; i++){
+            qualificationsPage.deleteWorkExperience();
+            Boolean flag = qualificationsPage.isWorkExperienceDeleted();
+            Assert.assertTrue(flag);
+            Thread.sleep(3000);
+        }
     }
 
     @AfterMethod
