83af1e1dbc80d9bc1d17df0b3b008383561b44d5
kaflemahesh <mahesh.kafle@smarsh.com>
2021-09-26 16:12:55 +0545

    Implemented ESS user can add Memberships Record

--- src/test/java/com/orangehrm/qa/testcases/MembershipsPageTest.java
+++ src/test/java/com/orangehrm/qa/testcases/MembershipsPageTest.java
@@ -3,8 +3,10 @@ package com.orangehrm.qa.testcases;
 import com.orangehrm.qa.base.TestBase;
 import com.orangehrm.qa.pages.DashboardPage;
 import com.orangehrm.qa.pages.LoginPage;
+import com.orangehrm.qa.pages.MembershipsPage;
 import com.orangehrm.qa.pages.PersonalDetailsPage;
 import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -15,6 +17,7 @@ public class MembershipsPageTest extends TestBase {
     PersonalDetailsPage personalDetailsPage;
     DashboardPage dashboardPage;
     LoginPage loginPage;
+    MembershipsPage membershipsPage;
 
     public MembershipsPageTest(){
         super();
@@ -26,18 +29,30 @@ public class MembershipsPageTest extends TestBase {
         loginPage = new LoginPage();
         dashboardPage = new DashboardPage();
         personalDetailsPage = new PersonalDetailsPage();
+        membershipsPage = new MembershipsPage();
         dashboardPage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
         personalDetailsPage = dashboardPage.clickOnPersonalDetailsPageLink();
+        JavascriptExecutor js = ((JavascriptExecutor) driver);
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        membershipsPage = personalDetailsPage.clickOnMembershipsPageLink();
+
     }
 
     @Test(priority = 1)
-    public void verifyNavigateToJobPage() throws InterruptedException {
-
-        JavascriptExecutor js = ((JavascriptExecutor) driver);
-        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-
-        personalDetailsPage.clickOnMembershipsPageLink();
-        Thread.sleep(3000);
+    public void verifyAddMembershipDetails() throws InterruptedException {
+        for(int i=0; i<2; i++){
+            membershipsPage.ClickBtnAddMembershipDetail();
+            membershipsPage.AddMembership(prop.getProperty("valueMembership"));
+            membershipsPage.AddSubscriptionPaidBy(prop.getProperty("valueSubscriptionPaidBy"));
+            membershipsPage.AddSubscriptionAmount(prop.getProperty("valueSubscriptionAmount"));
+            membershipsPage.AddMembershipCurrency(prop.getProperty("valueMembershipCurrency"));
+            membershipsPage.AddSubscriptionCommenceDate(prop.getProperty("valueSubscriptionCommenceDate"));
+            membershipsPage.AddSubscriptionRenewalDate(prop.getProperty("valueSubscriptionRenewalDate"));
+            membershipsPage.ClickOnBtnSaveMembership();
+            boolean flag = membershipsPage.IsMembershipsAdded();
+            Assert.assertTrue(flag);
+            Thread.sleep(3000);
+        }
     }
 
     @AfterMethod
