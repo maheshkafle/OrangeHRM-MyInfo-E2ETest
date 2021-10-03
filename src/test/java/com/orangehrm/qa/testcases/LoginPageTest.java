ebd763f54449664718387c2734a9283d41782280
kaflemahesh <mahesh.kafle@smarsh.com>
2021-10-03 15:43:19 +0545

    Added proper logs using log4j Api

--- src/test/java/com/orangehrm/qa/testcases/LoginPageTest.java
+++ src/test/java/com/orangehrm/qa/testcases/LoginPageTest.java
@@ -4,6 +4,7 @@ import com.orangehrm.qa.base.TestBase;
 import com.orangehrm.qa.pages.DashboardPage;
 import com.orangehrm.qa.pages.LoginPage;
 import com.orangehrm.qa.utils.TestUtil;
+import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -17,6 +18,8 @@ public class LoginPageTest extends TestBase {
     DashboardPage dashboardPage;
 
     String sheetName = "credentials";
+
+    Logger log = Logger.getLogger(TestBase.class);
 
     public LoginPageTest(){
         // Call TestBase class constructor which initializes all properties
@@ -37,22 +40,31 @@ public class LoginPageTest extends TestBase {
 
     @Test(priority = 1)
     public void loginPageTitleTest() throws InterruptedException {
+        log.info("************************Before loginPageTitleTest *******************************");
+        log.warn("************************ Hey this is warning message *******************************");
+        log.debug("************************ Hey this is debug message *******************************");
+        log.fatal("************************ Hey this is fatal error message *******************************");
         String title = loginPage.validateLoginPageTitle();
         Thread.sleep(3000);
         System.out.println("Actual title"+title);
         Assert.assertEquals(title, "OrangeHRM");
+        log.info("************************After loginPageTitleTest *******************************");
     }
 
     @Test(priority = 2)
     public void testValidateLoginPanel() {
+        log.info("************************Before ValidateLoginPanel *******************************");
         boolean flag = loginPage.validateLoginPanel();
         Assert.assertTrue(flag);
+        log.info("************************After ValidateLoginPanel *******************************");
     }
 
     @Test(priority = 3, dataProvider="getOrangeHRMLoginData")
     public void loginTest(String username, String password) {
         //dashboardPage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+        log.info("************************Before Login*******************************");
         dashboardPage = loginPage.login(username, password);
+        log.info("************************After Login*******************************");
     }
 
     @AfterMethod
